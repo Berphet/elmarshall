0e8727c5fcb815592777f49b5172fc20e0d16b78
Marshall Gallardo <82956039+Berphet@users.noreply.github.com>
2021-06-24 14:03:14 -0400

    Update deleteContenido.php

--- acumulativa2/deleteContenido.php
+++ acumulativa2/deleteContenido.php
@@ -23,7 +23,7 @@ if(isset($_POST['delete'])){
         $result = mysqli_query($conn,$sql_query);
 
         if($result > 0){
-            header('Location: showContenido.php');
+            header('Location: index.php');
         }else{
             echo "Intente nuevamente";
         }
@@ -57,7 +57,7 @@ if(isset($_POST['delete'])){
                             
                 <button class="btn btn-success" name="delete">Eliminar Contenido</button>
                 </form>
-                <button style="margin-top: 1%" onclick="window.location='showContenido.php'">Volver</button>
+                <button style="margin-top: 1%" onclick="window.location='index.php'">Volver</button>
             </div>
             
         </div>
